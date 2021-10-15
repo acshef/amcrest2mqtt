e4fcb9a3539394a48e0e5899339c177c793a04f4
Aiden Shef <acshef+github@gmail.com>
2021-10-15 17:50:26 -0600

    Use new Config, Device, Entity, MQTTClient classes in Amcreste2MQTT class main logic

--- amcrest2mqtt/amcrest2mqtt.py
+++ amcrest2mqtt/amcrest2mqtt.py
@@ -1,19 +1,18 @@
-from json import dumps
 import logging
 import os
 import signal
-import ssl
 import sys
 from threading import Timer
 import typing as t
 
 from amcrest import AmcrestCamera, AmcrestError
-import paho.mqtt.client as mqtt
 
 from .config import Config
 from .const import *
 from .device import Device
-from .util import ping
+from .entity import Entity
+from .mqtt_client import MQTTClient
+from .util import ping, slugify
 
 
 _is_exiting = False # Global
@@ -23,243 +22,78 @@ logger = logging.getLogger(__name__)
 
 
 class Amcrest2MQTT:
-    mqtt_client: t.Optional[mqtt.Client] = None
     def __init__(self):
         self.config = Config.from_env()
 
     def run(self):
+        from amcrest2mqtt import __version__
+        logger.info(f"{APP_NAME} v{__version__}")
+
         # Exit if any of the required vars are not provided
         if self.config.amcrest_host is None:
-            logger.error(f"Please set the {ENV_AMCREST_HOST} environment variable")
+            logger.error(f"Environment variable {ENV_AMCREST_HOST} must be set")
             sys.exit(1)
 
         if self.config.amcrest_password is None:
-            logger.error(f"Please set the {ENV_AMCREST_PASSWORD} environment variable")
+            logger.error(f"Environment variable {ENV_AMCREST_PASSWORD} must be set")
             sys.exit(1)
 
         if self.config.mqtt_username is None:
-            logger.error(f"Please set the {ENV_MQTT_USERNAME} environment variable")
+            logger.error(f"Environment variable {ENV_MQTT_USERNAME} must be set")
             sys.exit(1)
-
-        from amcrest2mqtt import __version__ as version
-        logger.info(f"{APP_NAME} v{version}")
 
         # Handle interruptions
         signal.signal(signal.SIGINT, self.signal_handler)
 
-        self.camera = AmcrestCamera(
-            self.config.amcrest_host,
-            self.config.amcrest_port,
-            self.config.amcrest_username,
-            self.config.amcrest_password
-        ).camera
+        self.camera = AmcrestCamera(*self.config.amcrest_camera_args).camera
 
         logger.info("Fetching camera details")
-
         try:
-            device_type = self.camera.device_type.replace("type=", "").strip()
-            serial_number = self.camera.serial_number.strip()
-            sw_version = self.camera.software_information[0].replace("version=", "").strip()
-            device_name = self.camera.machine_name.replace("name=", "").strip()
+            self.device = Device.from_amcrest_camera(self.camera)
         except AmcrestError:
             logger.error(f"Error fetching camera details")
-            self.exit_gracefully(1)
-
-        self.device = Device(
-            name=device_name,
-            model=device_type,
-            serial_no=serial_number,
-            sw_version=sw_version
-        )
+            sys.exit(1)
 
         logger.info(f"Device: {self.device.manufacturer} {self.device.model} {self.device.name}")
         logger.info(f"Serial number: {self.device.serial_no}")
         logger.info(f"Software version: {self.device.sw_version}")
 
-        a2m_topic_prefix = f"amcrest2mqtt/{self.device.serial_no}"
-        node_id = f"amcrest2mqtt-{self.device.serial_no}"
-
-        # MQTT topics
-        self.topics = {
-            # "config": f"amcrest2mqtt/{device.serial_no}/config",
-            "status":               f"{a2m_topic_prefix}/status",
-            "event":                f"{a2m_topic_prefix}/event",
-            "motion":               f"{a2m_topic_prefix}/motion",
-            "doorbell":             f"{a2m_topic_prefix}/doorbell",
-            "human":                f"{a2m_topic_prefix}/human",
-            "light":                f"{a2m_topic_prefix}/light",
-            "storage_used":         f"{a2m_topic_prefix}/storage/used",
-            "storage_used_percent": f"{a2m_topic_prefix}/storage/used_percent",
-            "storage_total":        f"{a2m_topic_prefix}/storage/total",
-            "home_assistant": {
-                "doorbell":             f"{self.config.ha_prefix}/binary_sensor/{node_id}/{self.device.slug}_doorbell/config",
-                "human":                f"{self.config.ha_prefix}/binary_sensor/{node_id}/{self.device.slug}_human/config",
-                "motion":               f"{self.config.ha_prefix}/binary_sensor/{node_id}/{self.device.slug}_motion/config",
-                "light":                f"{self.config.ha_prefix}/light/{node_id}/{self.device.slug}_light/config",
-                "storage_used":         f"{self.config.ha_prefix}/sensor/{node_id}/{self.device.slug}_storage_used/config",
-                "storage_used_percent": f"{self.config.ha_prefix}/sensor/{node_id}/{self.device.slug}_storage_used_percent/config",
-                "storage_total":        f"{self.config.ha_prefix}/sensor/{node_id}/{self.device.slug}_storage_total/config",
-            },
-        }
-
-        self.mqtt_client = mqtt.Client(
-            client_id=f"amcrest2mqtt_{self.device.serial_no}", clean_session=False
-        )
-        self.mqtt_client.on_disconnect = self.on_mqtt_disconnect
-        self.mqtt_client.will_set(self.topics["status"], payload=PAYLOAD_OFFLINE, qos=self.config.mqtt_qos, retain=True)
-        if self.config.mqtt_tls_enabled:
-            logger.info(f"Setting up MQTT for TLS")
-            if self.config.mqtt_tls_ca_cert is None:
-                logger.error(f"Missing environment variable: {ENV_MQTT_TLS_CA_CERT}")
-                sys.exit(1)
-            if self.config.mqtt_tls_cert is None:
-                logger.error(f"Missing environment variable: {ENV_MQTT_TLS_CERT}")
-                sys.exit(1)
-            if self.config.mqtt_tls_cert is None:
-                logger.error(f"Missing environment variable: {ENV_MQTT_TLS_KEY}")
-                sys.exit(1)
-            self.mqtt_client.tls_set(
-                ca_certs=self.config.mqtt_tls_ca_cert,
-                certfile=self.config.mqtt_tls_cert,
-                keyfile=self.config.mqtt_tls_key,
-                cert_reqs=ssl.CERT_REQUIRED,
-                tls_version=ssl.PROTOCOL_TLS,
-            )
-        else:
-            self.mqtt_client.username_pw_set(
-                username=self.config.mqtt_username,
-                password=self.config.mqtt_password
-            )
-
         try:
-            self.mqtt_client.connect(self.config.mqtt_host, port=self.config.mqtt_port)
-            self.mqtt_client.loop_start()
-        except ConnectionError as error:
-            logger.error(f"Could not connect to MQTT server: {error}")
+            self.mqtt_client = MQTTClient(config=self.config, device=self.device)
+            self.mqtt_client.on_disconnect = self.on_mqtt_disconnect
+        except Exception as exc:
+            logger.error(f"Could not connect to MQTT server: {exc}")
             sys.exit(1)
+
+        # Create entities
+        self.entity_doorbell = self.create_entity(**Entity.DEF_DOORBELL)
+        self.entity_human = self.create_entity(**Entity.DEF_HUMAN)
+        self.entity_flashlight = self.create_entity(**Entity.DEF_FLASHLIGHT)
+        self.entity_motion = self.create_entity(**Entity.DEF_MOTION)
+        self.entity_storage_used_percent = self.create_entity(**Entity.DEF_STORAGE_USED_PERCENT)
+        self.entity_storage_used = self.create_entity(**Entity.DEF_STORAGE_USED)
+        self.entity_storage_total = self.create_entity(**Entity.DEF_STORAGE_TOTAL)
 
         # Configure Home Assistant
         if self.config.ha_enabled:
             logger.info("Writing Home Assistant discovery config...")
 
-            base_config = {
-                "availability_topic": self.topics["status"],
-                "device": self.device.as_mqtt_device_dict()
-            }
-
             if self.is_doorbell:
-                self.mqtt_publish(
-                    self.topics["home_assistant"]["doorbell"],
-                    base_config
-                    | {
-                        "state_topic": self.topics["doorbell"],
-                        "payload_on": PAYLOAD_ON,
-                        "payload_off": PAYLOAD_OFF,
-                        "name": f"{device_name} Doorbell",
-                        "unique_id": f"{self.device.serial_no}.doorbell",
-                    },
-                    json=True,
-                )
+                self.entity_doorbell.discover()
 
             if self.is_ad410:
-                self.mqtt_publish(
-                    self.topics["home_assistant"]["human"],
-                    base_config
-                    | {
-                        "state_topic": self.topics["human"],
-                        "payload_on": PAYLOAD_ON,
-                        "payload_off": PAYLOAD_OFF,
-                        "device_class": "motion",
-                        "name": f"{device_name} Human",
-                        "unique_id": f"{self.device.serial_no}.human",
-                        "icon": ICON_FACE_RECOGNITION,
-                    },
-                    json=True,
-                )
-                self.mqtt_publish(
-                    self.topics["home_assistant"]["light"],
-                    base_config
-                    | {
-                        "~": self.topics["light"],
-                        "state_topic": "~/state",
-                        "command_topic": "~/set",
-                        "effect_command_topic": "~/set_effect",
-                        "effect_state_topic": "~/effect",
-                        "effect_list": [
-                            LIGHT_MODE_SOLID,
-                            LIGHT_MODE_STROBE
-                        ],
-                        "payload_on": PAYLOAD_ON,
-                        "payload_off": PAYLOAD_OFF,
-                        "name": f"{device_name} Flashlight",
-                        "unique_id": f"{self.device.serial_no}.light",
-                        "icon": ICON_FLASHLIGHT,
-                    },
-                    json=True
-                )
+                self.entity_human.discover()
+                self.entity_flashlight.discover()
 
-            self.mqtt_publish(
-                self.topics["home_assistant"]["motion"],
-                base_config
-                | {
-                    "state_topic": self.topics["motion"],
-                    "payload_on": PAYLOAD_ON,
-                    "payload_off": PAYLOAD_OFF,
-                    "device_class": "motion",
-                    "name": f"{device_name} Motion",
-                    "unique_id": f"{self.device.serial_no}.motion",
-                },
-                json=True,
-            )
+            self.entity_motion.discover()
 
             if self.config.storage_poll_interval > 0:
-                self.mqtt_publish(
-                    self.topics["home_assistant"]["storage_used_percent"],
-                    base_config
-                    | {
-                        "state_topic": self.topics["storage_used_percent"],
-                        "unit_of_measurement": "%",
-                        "icon": ICON_MICRO_SD,
-                        "name": f"{device_name} Storage Used %",
-                        "unique_id": f"{self.device.serial_no}.storage_used_percent",
-                    },
-                    json=True,
-                )
+                self.entity_storage_used_percent.discover()
+                self.entity_storage_used.discover()
+                self.entity_storage_total.discover()
 
-                self.mqtt_publish(
-                    self.topics["home_assistant"]["storage_used"],
-                    base_config
-                    | {
-                        "state_topic": self.topics["storage_used"],
-                        "unit_of_measurement": "GB",
-                        "icon": ICON_MICRO_SD,
-                        "name": f"{device_name} Storage Used",
-                        "unique_id": f"{self.device.serial_no}.storage_used",
-                    },
-                    json=True,
-                )
-
-                self.mqtt_publish(
-                    self.topics["home_assistant"]["storage_total"],
-                    base_config
-                    | {
-                        "state_topic": self.topics["storage_total"],
-                        "unit_of_measurement": "GB",
-                        "icon": ICON_MICRO_SD,
-                        "name": f"{device_name} Storage Total",
-                        "unique_id": f"{self.device.serial_no}.storage_total",
-                    },
-                    json=True,
-                )
-        # Main loop
-        self.mqtt_publish(self.topics["status"], PAYLOAD_ONLINE)
-        # mqtt_publish(topics["config"], {
-        #     "version": __version__,
-        #     "device_type": device_type,
-        #     "device_name": device_name,
-        #     "sw_version": sw_version,
-        #     "serial_number": serial_number,
-        # }, json=True)
+        # Begin main behavior
+        self.mqtt_publish(self.device.status_topic, PAYLOAD_ONLINE)
 
         if self.config.storage_poll_interval > 0:
             logger.info("Performing initial check of storage sensors...")
@@ -271,29 +105,12 @@ class Amcrest2MQTT:
         logger.info("Listening for events...")
 
         try:
-            for code, payload in self.camera.event_actions("All", retries=5, timeout_cmd=(10.00, 3600)):
-                if code == ("ProfileAlarmTransmit" if self.is_ad110 else "VideoMotion"):
-                    motion_payload = PAYLOAD_ON if payload["action"] == "Start" else PAYLOAD_OFF
-                    self.mqtt_publish(self.topics["motion"], motion_payload)
-                elif code == "CrossRegionDetection" and payload["data"]["ObjectType"] == "Human":
-                    human_payload = PAYLOAD_ON if payload["action"] == "Start" else PAYLOAD_OFF
-                    self.mqtt_publish(self.topics["human"], human_payload)
-                elif code == "_DoTalkAction_":
-                    doorbell_payload = PAYLOAD_ON if payload["data"]["Action"] == "Invite" else PAYLOAD_OFF
-                    self.mqtt_publish(self.topics["doorbell"], doorbell_payload)
-                elif code == "LeFunctionStatusSync" and payload["data"]["Function"] == "WightLight":
-                    light_payload = PAYLOAD_ON if payload["data"]["Status"] == "true" else PAYLOAD_OFF
-                    light_mode = (
-                        LIGHT_MODE_STROBE
-                        if light_payload == PAYLOAD_ON and "true" in payload["data"]["Flicker"]
-                        else LIGHT_MODE_SOLID
-                    )
-                    self.mqtt_publish("{}/{}".format(self.topics["light"], "state"), light_payload)
-                    self.mqtt_publish("{}/{}".format(self.topics["light"], "effect"), light_mode)
-
-                self.mqtt_publish(self.topics["event"], payload, json=True)
-                logger.info(str(payload))
-
+            for code, payload in self.camera.event_actions(
+                CAMERA_EVENTS_SPECIFIER,
+                retries=CAMERA_EVENTS_RETRIES,
+                timeout_cmd=CAMERA_EVENTS_TIMEOUT
+            ):
+                self.handle_event(code, payload)
         except AmcrestError as error:
             logger.error(f"Amcrest error {error}")
             self.exit_gracefully(1)
@@ -315,21 +132,21 @@ class Amcrest2MQTT:
     def mqtt_publish(self, topic: str, payload: t.Any, exit_on_error=True, json=False):
         assert self.mqtt_client is not None
 
-        msg = self.mqtt_client.publish(
-            topic,
-            payload=(dumps(payload) if json else payload),
-            qos=self.config.mqtt_qos,
-            retain=True
+        try:
+            return self.mqtt_client.publish(topic, payload, json)
+        except Exception as exc:
+            logger.exception(exc)
+            if exit_on_error:
+                self.exit_gracefully(1, skip_mqtt=True)
+
+    def create_entity(self, name: str, component: str, *, friendly_name: str=None, **extra_config):
+        return Entity(
+            self,
+            name,
+            component,
+            friendly_name=friendly_name,
+            **extra_config
         )
-
-        if msg.rc == mqtt.MQTT_ERR_SUCCESS:
-            msg.wait_for_publish()
-            return msg
-
-        logger.error(f"Error publishing MQTT message: {mqtt.error_string(msg.rc)}")
-
-        if exit_on_error:
-            self.exit_gracefully(msg.rc, skip_mqtt=True)
 
     def on_mqtt_disconnect(self, client, userdata, rc: int):
         if rc != 0:
@@ -340,13 +157,37 @@ class Amcrest2MQTT:
         logger.info("Exiting app...")
 
         if self.mqtt_client is not None and self.mqtt_client.is_connected() and not skip_mqtt:
-            self.mqtt_publish(self.topics["status"], PAYLOAD_OFFLINE, exit_on_error=False)
+            if self.device:
+                self.mqtt_publish(self.device.status_topic, PAYLOAD_OFFLINE, exit_on_error=False)
             self.mqtt_client.loop_stop(force=True)
             self.mqtt_client.disconnect()
 
-        # Use os._exit instead of sys.exit to ensure an MQTT disconnect event causes the program to exit correctly
-        # as they occur on a separate thread
+        # Use os._exit instead of sys.exit to ensure an MQTT disconnect event
+        # causes the program to exit correctly as they occur on a separate thread
         os._exit(rc)
+
+    def handle_event(self, code, payload):
+        if code == ("ProfileAlarmTransmit" if self.is_ad110 else "VideoMotion"):
+            motion_payload = PAYLOAD_ON if payload["action"] == "Start" else PAYLOAD_OFF
+            self.entity_motion.publish(motion_payload)
+        elif code == "CrossRegionDetection" and payload["data"]["ObjectType"] == "Human":
+            human_payload = PAYLOAD_ON if payload["action"] == "Start" else PAYLOAD_OFF
+            self.entity_human.publish(human_payload)
+        elif code == "_DoTalkAction_":
+            doorbell_payload = PAYLOAD_ON if payload["data"]["Action"] == "Invite" else PAYLOAD_OFF
+            self.entity_doorbell.publish(doorbell_payload)
+        elif code == "LeFunctionStatusSync" and payload["data"]["Function"] == "WightLight":
+            light_payload = PAYLOAD_ON if payload["data"]["Status"] == "true" else PAYLOAD_OFF
+            light_mode = (
+                LIGHT_MODE_STROBE
+                if light_payload == PAYLOAD_ON and "true" in payload["data"]["Flicker"]
+                else LIGHT_MODE_SOLID
+            )
+            self.entity_flashlight.publish(light_payload)
+            self.entity_flashlight.publish(light_mode, "effect")
+
+        self.mqtt_publish(self.device.event_topic, payload, json=True)
+        logger.info(str(payload))
 
     def refresh_storage_sensors(self):
         Timer(self.config.storage_poll_interval, self.refresh_storage_sensors).start()
@@ -354,9 +195,9 @@ class Amcrest2MQTT:
 
         try:
             storage = self.camera.storage_all
-            self.mqtt_publish(self.topics["storage_used_percent"], str(storage["used_percent"]))
-            self.mqtt_publish(self.topics["storage_used"], str(storage["used"][0]))
-            self.mqtt_publish(self.topics["storage_total"], str(storage["total"][0]))
+            self.entity_storage_used_percent.publish(storage["used_percent"])
+            self.entity_storage_used.publish(storage["used"][0])
+            self.entity_storage_total.publish(storage["total"][0])
         except AmcrestError as error:
             logger.warning(f"Error fetching storage information: {error}")
 
