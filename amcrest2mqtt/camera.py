190ec0cc16e2b84ad9e20741507e48ee87174790
Aiden Shef <acshef+github@gmail.com>
2021-10-16 13:57:19 -0600

    Add Camera class (wraps AmcrestCamera().camera)

---
+++ amcrest2mqtt/camera.py
@@ -0,0 +1,56 @@
+import typing as t
+
+from amcrest import AmcrestCamera, AmcrestError
+
+from .config import Config
+from .const import *
+from .device import Device
+
+
+__all__ = ["Camera", "AmcrestError"]
+
+
+_T = t.TypeVar("_T")
+
+
+class Camera:
+	def __init__(self, *args, **kwargs):
+		self._camera = AmcrestCamera(*args, **kwargs).camera
+
+	def __getattr__(self, attr):
+		return getattr(self._camera, attr)
+
+	def get_config(self, name: str, type: t.Callable[[str], _T]=str) -> _T:
+		ret = self._camera.command(f"configManager.cgi?action=getConfig&name={name}")
+		line = ret.content.decode().strip() # Should be of the form "key.subkey.subsubkey=value"
+		_, _, value = line.partition("=")
+		return type(value.strip())
+
+	def set_config(self, name: str, value: t.Any):
+		ret = self._camera.command(f"configManager.cgi?action=setConfig&{name}={value}")
+		return "ok" in ret.content.decode().lower()
+
+	def get_device(self):
+		device_type = self._camera.device_type.replace("type=", "").strip()
+		serial_number = self._camera.serial_number.strip()
+		sw_version = self._camera.software_information[0].replace("version=", "").strip()
+		device_name = self._camera.machine_name.replace("name=", "").strip()
+
+		return Device(
+			name=device_name,
+            model=device_type,
+            serial_no=serial_number,
+            sw_version=sw_version
+		)
+
+	@classmethod
+	def from_config(cls, config: Config) -> "Camera":
+		return cls(config.amcrest_host, config.amcrest_port, config.amcrest_username, config.amcrest_password)
+
+	def events(self) -> t.Iterable[t.Tuple[str, dict]]:
+		for code, payload in self._camera.event_actions(
+			CAMERA_EVENTS_SPECIFIER,
+			retries=CAMERA_EVENTS_RETRIES,
+			timeout_cmd=CAMERA_EVENTS_TIMEOUT
+		):
+			yield code, payload
