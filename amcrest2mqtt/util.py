e527546125f6fd841ed0fdb6ad3d332bf7d51f81
Aiden Shef <acshef+github@gmail.com>
2022-02-13 15:33:40 -0700

    Use explicit falsy values in str2bool() utility

--- amcrest2mqtt/util.py
+++ amcrest2mqtt/util.py
@@ -12,24 +12,24 @@ _T = t.TypeVar("_T", int, float)
 
 
 def ping(host: str, timeout: t.Union[float, int, timedelta] = None) -> bool:
-	if isinstance(timeout, timedelta):
-		timeout = timeout.total_seconds()
+    if isinstance(timeout, timedelta):
+        timeout = timeout.total_seconds()
 
-	count_param = "-n" if SYS_WINDOWS else "-c"
-	command = ["ping", count_param, "1"]
-	if timeout:
-		if SYS_WINDOWS:
+    count_param = "-n" if SYS_WINDOWS else "-c"
+    command = ["ping", count_param, "1"]
+    if timeout:
+        if SYS_WINDOWS:
             timeout *= 1000  # Convert to milliseconds
-		command += ["-w" if SYS_WINDOWS else "-W", str(timeout)]
-	command += [host]
+        command += ["-w" if SYS_WINDOWS else "-W", str(timeout)]
+    command += [host]
 
     p = subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-	return p.returncode == 0
+    return p.returncode == 0
 
 
 def slugify(text: str) -> str:
-	return _slugify(text, separator="_")
+    return _slugify(text, separator="_")
 
 
 def clamp(
@@ -38,12 +38,14 @@ def clamp(
     min: t.Optional[t.Union[int, float]] = None,
     max: t.Optional[t.Union[int, float]] = None,
 ) -> _T:
-	if min is not None and value < min:
-		value = min
-	if max is not None and value > max:
-		value = max
-	return value
+    if min is not None and value < min:
+        value = min
+    if max is not None and value > max:
+        value = max
+    return value
 
 
 def str2bool(value: t.Any) -> bool:
-	return str(value).lower() == "true"
+    if value is None:
+        return False
+    return str(value).lower().strip() not in ("no", "off", "false", "0", "")
